6d596710ecdaf3470596c736844610b23a3c051b
fadhil aly <alyfadhil24@gmail.com>
2025-03-24 10:13:16 +0700

    create connection to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-import express, { json } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+const env = dotenv.config().parsed;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  console.log("hello world");
+mongoose.connect(`${env.MONGODB_URI}/${env.DB_NAME}`, {
+  dbName: env.DB_NAME,
 });
 
-app.listen(3000, () => {
-  console.log(`listening on port : 3000`);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error : "));
+db.once("open", () => {
+  console.log(`Connected to MongoDB : ${env.DB_NAME}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: `Hello ${req.body.name}`,
+  });
+});
+
+app.listen(env.PORT, () => {
+  console.log(`listening on port : ${env.PORT}`);
 });
